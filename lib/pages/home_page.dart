3ab7ceb67bad7674f718f54009373f758716f8fd
Varun Tandon <varuntandon28121993@gmail.com>
2021-06-02 00:33:14 +0530

    add items - decode json to map into items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    final productsData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productsData).map((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App")
-      ),
+      appBar: AppBar(title: Text("Catalog App")),
       body: Center(
         child: Material(
           child: Container(
-            child: Text("Welcome to $days days of Flutter"),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(item: CatalogModel.items[index]);
+                },
+              ),
+            ),
           ),
         ),
       ),
